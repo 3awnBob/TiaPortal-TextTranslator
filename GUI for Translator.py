8b84e99e0d8faf0f4534050bb30bc4ecbb61bcb9
3awnBob <120493458+3awnBob@users.noreply.github.com>
2025-03-25 09:05:45 +0100

    Update GUI for Translator.py
    Cleanup

--- GUI for Translator.py
+++ GUI for Translator.py
@@ -1,12 +1,4 @@
-"""
 
-Copyright (c) 2024 Kais K. All rights reserved.
-
-This script is proprietary and confidential and may be protected by copyright law.
-You may not reproduce, distribute, or modify this code without the express written
-permission of the copyright holder.
-
-"""
 import traceback
 import tkinter as tk
 from tkinter import filedialog
